1347cdceb068aa14a57704091819a34b6f13ec48
AnonymeUSBC <timothee.ren@proton.me>
2026-02-18 08:21:23 +0100

    🐛 (users) Fix queue name

--- src/main/java/com/cavehosting/api_gateway/Users/RabbitmqConfig.java
+++ src/main/java/com/cavehosting/api_gateway/Users/RabbitmqConfig.java
@@ -4,15 +4,14 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import org.springframework.stereotype.Component;
 import org.springframework.amqp.core.Queue;
 
-@Configuration
+@Component
 public class RabbitmqConfig {
   @Bean
   public Queue userQueu() {
-    return new Queue("users.queu", true);
+    return new Queue("users.queue", true);
   }
 
   @Bean
